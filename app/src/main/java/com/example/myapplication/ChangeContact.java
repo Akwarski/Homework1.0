cee3f2500b48cf87334c06fca1079af96199ebad
GawronskiTomasz <gawronski.tomaszz@gmail.co>
2019-04-01 18:43:38 +0200

    Bo tak

--- app/src/main/java/com/example/myapplication/ChangeContact.java
+++ app/src/main/java/com/example/myapplication/ChangeContact.java
@@ -1,4 +1,79 @@
 package com.example.myapplication;
 
-public class ChangeContact   {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class ChangeContact extends AppCompatActivity {
+    String[] tabString;
+    RadioButton rb1, rb2, rb3, rb4, rb5;
+    Button cancel;
+    boolean checked = false;
+    String nameSet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_contact);
+        tabString = getResources().getStringArray(R.array.contacts);
+        rb1 = findViewById(R.id.RadioButton1);
+        rb1.setText(new String((Base64.decode(tabString[0],Base64.DEFAULT))));
+        rb2 = findViewById(R.id.RadioButton2);
+        rb2.setText(new String((Base64.decode(tabString[1],Base64.DEFAULT))));
+        rb3 = findViewById(R.id.RadioButton3);
+        rb3.setText(new String((Base64.decode(tabString[2],Base64.DEFAULT))));
+        rb4 = findViewById(R.id.RadioButton4);
+        rb4.setText(new String((Base64.decode(tabString[3],Base64.DEFAULT))));
+        rb5 = findViewById(R.id.RadioButton5);
+        rb5.setText(new String((Base64.decode(tabString[4],Base64.DEFAULT))));
+        cancel = findViewById(R.id.CANCEL);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public void confirm(View view) {
+        if(checked) {
+            Intent intent = new Intent();
+            intent.putExtra("nameSet", nameSet);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Select contact", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void chooseName(View view) {
+        checked = ((RadioButton) view).isChecked();
+
+        if(checked) {
+            switch (view.getId()) {
+                case R.id.RadioButton1:
+                    nameSet = tabString[0];
+                    break;
+                case R.id.RadioButton2:
+                    nameSet = tabString[1];
+                    break;
+                case R.id.RadioButton3:
+                    nameSet = tabString[2];
+                    break;
+                case R.id.RadioButton4:
+                    nameSet = tabString[3];
+                    break;
+                case R.id.RadioButton5:
+                    nameSet = tabString[4];
+                    break;
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv;
+    ImageView iv;
+    int drawable, rand = 0, sound = 0, flag = 0, temporary;
+    String name = "", temp = "";
+    MediaPlayer mp;
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +35,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        iv = findViewById(R.id.imageView);
+        tv = findViewById(R.id.textView);
+
+        randNum();
+
+        final FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                if(mp != null){
+                    mp.stop();
+                    mp = null;
+                }else{
+                    mp = MediaPlayer.create(getApplicationContext(), sound);
+                    mp.start();
+
+                    Snackbar.make(view, "playing...", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
             }
         });
     }
@@ -48,5 +79,86 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void randNum(){
+        Random r = new Random();
+        rand = r.nextInt(5);
+        choosePicture(rand);
+    }
+
+    public void choosePicture(int i){
+        switch (i){
+            case 0 :
+                drawable = R.drawable.avatar_1;
+                break;
+            case 1:
+                drawable = R.drawable.avatar_2;
+                break;
+            case 2:
+                drawable = R.drawable.avatar_3;
+                break;
+            case 3:
+                drawable = R.drawable.avatar_4;
+                break;
+            case 4:
+                drawable = R.drawable.avatar_5;
+                break;
+        }
+        iv.setImageResource(drawable);
+    }
+
+    public void changeContact(View view) {
+        Intent intent = new Intent(getApplicationContext(), ChangeContact.class);
+        startActivityForResult(intent,1);
+    }
+
+    public void changeSound(View view) {
+        if(!tv.getText().equals("")) {
+            Intent intent = new Intent(getApplicationContext(), ChangeSound.class);
+            startActivityForResult(intent, 2);
+        }else
+            Toast.makeText(this, "First choose contact", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                temp = name;
+                name = data.getStringExtra("nameSet");
+
+                if(!temp.equals(name)){
+                    tv.setText(new String((Base64.decode(name,Base64.DEFAULT))));
+                    randNum();
+                }
+
+            }else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(), "I'm back!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if(requestCode == 2){
+            if(resultCode == RESULT_OK){
+                sound = data.getIntExtra("sound", 0);
+            }else if(resultCode == RESULT_CANCELED){
+                Toast.makeText(getApplicationContext(), "I'm back!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void stopPlayer(){
+        if(mp != null){
+            mp.release();
+            mp = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopPlayer();
     }
 }
